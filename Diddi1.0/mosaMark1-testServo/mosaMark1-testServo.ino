df6ed011d083d1946b08f1248e494d945264f206
Didier Mupenda <didiermupenda@dhcp-10-24-99-202.wlan.ntnu.no>
2023-09-20 17:28:01 +0200

    lagt til mosa2.1 fil

--- Diddi1.0/mosaMark1-testServo/mosaMark1-testServo.ino
+++ Diddi1.0/mosaMark1-testServo/mosaMark1-testServo.ino
@@ -11,6 +11,9 @@ Servo s3;
 Servo s4;
 Servo s5;
 
+unsigned long previousMillis = 0;
+const long interval = 10; // Tidsintervall for hvert servosteg (juster etter behov)
+
 int position;
 
 void setup() {
@@ -34,23 +37,30 @@ void setup() {
 
 
 void loop() {
-  
-  /*Plukk og dropp fuksjon*/
-  Serial.println("Plukke-funksjonen");
-  ArmDown();
-  delay(800);
-  PickUp();
-  ArmUp();
-  Serial.println("Slutt prosess");
-  Serial.println("Venter...");
-  delay(200);
-  Serial.println("Drop-funksjonen");
-  TurnToDrop();
-  DropIt();
-  ArmUp();
+
   RestPos();
-  Serial.println("Utført drop funksjoner");
-  delay(800);
+  delay(200);
+  TurnToDrop();
+  delay(200);
+  RestPos();
+  delay(200);
+  
+  // /*Plukk og dropp fuksjon*/
+  // Serial.println("Plukke-funksjonen");
+  // ArmDown();
+  // delay(800);
+  // PickUp();
+  // ArmUp();
+  // Serial.println("Slutt prosess");
+  // Serial.println("Venter...");
+  // delay(200);
+  // Serial.println("Drop-funksjonen");
+  // TurnToDrop();
+  // DropIt();
+  // ArmUp();
+  // RestPos();
+  // Serial.println("Utført drop funksjoner");
+  // delay(800);
 
   // Input fra bruker
   if (Serial.available()){
@@ -100,38 +110,53 @@ void loop() {
   }
 }
 
-
-
-void SoftMove(Servo servo, int targetPos){
-  int step = 0;
+void SoftMove(Servo servo, int targetPos) {
   int currentPos = servo.read();
-  // Rotasjon mot klokka
-  if(targetPos > currentPos){ 
-    step = (targetPos - currentPos > 0) ? 1 : 0;
-    for(int pos = currentPos; pos < targetPos; pos += step){
-      servo.write(pos);
-      delay(10);
-    }
-  } // Rotasjon med klokka
-  else{
-    step = (currentPos - targetPos > 0) ? 1 : 0;  
-    for(int pos = currentPos; pos > targetPos; pos -= step){
-      servo.write(pos);
-      delay(10);
+  if (currentPos != targetPos) {
+    unsigned long currentMillis = millis();
+    if (currentMillis - previousMillis >= interval) {
+      previousMillis = currentMillis;
+      int step = (targetPos > currentPos) ? 1 : 0;
+      int newPos = currentPos + step;
+      servo.write(newPos);
     }
   }
-  servo.write(targetPos);
 }
 
-void RestPos(){
-  SoftMove(s1, 60);
-  SoftMove(s2, 130);
-  delay(200);
-  SoftMove(s3, 40);
-  delay(200);
-  s4.write(0); 
-  s5.write(180);
+
+void TurnToDrop(){
+  SoftMove(s1, 180);
+  SoftMove(s3, 120);
+  SoftMove(s2, 40);
+  SoftMove(s3, 90);
   delay(800);
+  
+  // s1.write(180);
+  // s3.write(120);
+  // s2.write(40);
+  // s3.write(90);
+  // delay(800);
+}
+
+
+void RestPos(){
+  // SoftMove(s1, 60);
+  // SoftMove(s2, 130);
+  // delay(200);
+  // SoftMove(s3, 40);
+  // delay(200);
+  // s4.write(0); 
+  // s5.write(180);
+  // delay(800);
+
+  // s1.write(60);
+  // s2.write(130);
+  // delay(200);
+  // s3.write(40);
+  // delay(200);
+  // s4.write(0); 
+  // s5.write(180);
+  // delay(800);
 }
 
 void ArmUp(){
@@ -146,14 +171,6 @@ void ArmDown(){
   delay(200);
   SoftMove(s3, 130);
   SoftMove(s2, 0);
-}
-
-void TurnToDrop(){
-  SoftMove(s1, 180);
-  SoftMove(s3, 120);
-  SoftMove(s2, 40);
-  SoftMove(s3, 90);
-  delay(800);
 }
 
 void DropIt(){
@@ -171,15 +188,23 @@ void PickUp(){
   delay(800);
 }
 
-void LockPos(){
-  SoftMove(s1, 60);
-  SoftMove(s2, 120);
-  delay(200);
-  SoftMove(s3, 0);
-  delay(200);
-  s4.write(0); 
-  s5.write(180);
-  delay(800);
-}
-
-
+// void SoftMove(Servo servo, int targetPos){
+//   int step = 0;
+//   int currentPos = servo.read();
+//   // Rotasjon mot klokka
+//   if(targetPos > currentPos){ 
+//     step = (targetPos - currentPos > 0) ? 1 : 0;
+//     for(int pos = currentPos; pos < targetPos; pos += step){
+//       servo.write(pos);
+//       delay(10);
+//     }
+//   } // Rotasjon med klokka
+//   else{
+//     step = (currentPos - targetPos > 0) ? 1 : 0;  
+//     for(int pos = currentPos; pos > targetPos; pos -= step){
+//       servo.write(pos);
+//       delay(10);
+//     }
+//   }
+//   servo.write(targetPos);
+// }
